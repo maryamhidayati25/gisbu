8893b1cc5be6470e5433c7da1936d7861c758023
den_pras <pras.rubicteam@gmail.com>
2024-02-26 22:10:03 +0700

    maps leafet

--- resources/views/filament/pages/maps.blade.php
+++ resources/views/filament/pages/maps.blade.php
@@ -1,3 +1,32 @@
 <x-filament-panels::page>
-    
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin=""/>
+<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
+
+<div id="map" style="width: auto; height: 450px;"></div>
+
+<script>
+
+    const map = L.map('map').setView([-7.2996, 112.7637], 13);
+
+    const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+    const marker1 = L.marker([-7.2757340, 112.7204266]).addTo(map)
+        .bindPopup('<b>Hallo !</b><br />I am smk bahrul ulum.').openPopup();
+
+    const marker2 = L.marker([-7.2933975, 112.8001660]).addTo(map)
+        .bindPopup('<b>Hallo !</b><br />I am smkn 10 surabaya.').openPopup();
+
+    function onMapClick(e) {
+        popup
+            .setLatLng(e.latlng)
+            .setContent(`You clicked the map at ${e.latlng.toString()}`)
+            .openOn(map);
+    }
+
+    map.on('click', onMapClick);
+
+</script>
 </x-filament-panels::page>
