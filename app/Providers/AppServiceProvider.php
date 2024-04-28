6b93b0441bef12ad7b4fe2a2c3c579ff9ee84d0b
den_pras <pras.rubicteam@gmail.com>
2024-04-28 22:53:42 +0700

    initial commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,33 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
+     *
+     * @return void
      */
-    public function register(): void
+    public function register()
     {
         //
     }
 
     /**
      * Bootstrap any application services.
+     *
+     * @return void
      */
-    public function boot(): void
+    public function boot()
     {
         //
+        Schema::defaultStringLength(255);
+        // Paginator::defaultView('pagination::default');
+        Paginator::useBootstrap();
+        // Paginator::useTailwind();
     }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,19 +4,29 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
             $table->string('name');
+            $table->string('username')->unique();
+            $table->string('id_gmail')->unique()->nullable();
             $table->string('email')->unique();
+            $table->char('n_wa', 16)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('foto')->nullable();
+            $table->boolean('is_admin')->nullable();
+            $table->boolean('is_guru')->nullable();
+            $table->boolean('is_industri')->nullable();
+            $table->boolean('is_siswa')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
@@ -24,9 +34,11 @@ return new class extends Migration
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('users');
     }
-};
+}
